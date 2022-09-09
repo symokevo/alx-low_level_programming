d89fc3830d7744a2ec4a09a3dfde0ca3db1b6cef
Simon <symokevo@gmail.com>
2022-09-08 20:03:17 -0700

    create printf cfile

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+/**
+ *  *  *main - the entry point for the code
+ *   *
+ *    *  Return: 0 if exited properly, non-zero otherwise
+ *     */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+				return (0);
+}
