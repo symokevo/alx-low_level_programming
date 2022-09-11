f0c8417cd2d1118cf493ea953d4b65e236df45b9
zimaster <symokevo@gmail.com>
2022-09-11 17:44:39 +0300

    fix 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  *  *main - the entry point for the code
- *   *
- *    *  Return: 0 if exited properly, non-zero otherwise
- *     */
+ *  main - this is the entry point for the code
+ *
+ *  Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-				return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
