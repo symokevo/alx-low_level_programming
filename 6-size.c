89225a93234a98af9f139b62ba38dbf6a5921841
zimaster <symokevo@gmail.com>
2022-09-11 17:47:30 +0300

    create size.c file

---
+++ 6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point for the code.
+ *
+ * Return: zero if no error or non-zero values if error occurs.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
