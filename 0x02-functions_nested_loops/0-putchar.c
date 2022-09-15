ab758f71839a102ef87bc780d40246f122378d09
zimaster <symokevo@gmail.com>
2022-09-16 01:35:26 +0300

    create 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints putchar,
+ * a new line follows,
+ * return: Always 0 (if successful)
+ **/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
