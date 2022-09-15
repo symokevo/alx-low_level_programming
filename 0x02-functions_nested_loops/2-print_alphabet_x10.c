d661986962c8e4b707b9ad99a7ef35822516737d
zimaster <symokevo@gmail.com>
2022-09-16 02:44:37 +0300

    create print_alphabet_10x.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 -> Prints 10 times the alphabet, in small caps.
+*/
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
