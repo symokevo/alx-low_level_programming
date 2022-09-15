fc01d4163e9d5104d78834d043f3d3e580d5a5ef
zimaster <symokevo@gmail.com>
2022-09-16 02:42:10 +0300

    create alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet,
+ * In lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+
+	{
+
+	_putchar(ch);
+
+	ch++;
+
+	}
+
+	_putchar('\n');
+
+}
