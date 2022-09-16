564e1c9d2be9dec3186bdfad9e0107cefebcba52
zimaster <symokevo@gmail.com>
2022-09-16 03:59:17 +0300

    create print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers, from 0 - 9,
+ * a new line follows
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
