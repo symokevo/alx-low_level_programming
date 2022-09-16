b985e6489188da978ddd7ae5188b99ba1c7561d3
zimaster <symokevo@gmail.com>
2022-09-16 03:11:55 +0300

    create fibonacci.c file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include"stdio.h"
+#include"main.h"
+
+/**
+ * main - program Entry point
+ * Print the first 50 Fibonacci numbers
+ *              starting with 1 and 2 folled by a new line
+ * Return: Always 0 (if Successful)
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for
+	(count = 0;
+	 count < 50;
+	 ++count)
+
+	{
+			 sum = fib1 + fib2;
+			 printf("%lu", sum);
+
+			 fib1 = fib2;
+			 fib2 = sum;
+
+if (count == 49)
+	printf("\n");
+
+	else
+				printf(", ");
+
+	}
+
+	return (0);
+}
