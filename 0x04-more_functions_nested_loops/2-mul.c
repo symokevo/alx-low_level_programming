5663957cf33fb9798e226474685eb4cb66c06637
zimaster <symokevo@gmail.com>
2022-09-16 03:57:25 +0300

    create mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multipy 2 integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the results
+ */
+int mul(int a, int b)
+{
+	int c;
+
+		c = a * b;
+		return (c);
+}
