ed65ad599dd035f6e95a942a69d51e3db2ed4c39
zimaster <symokevo@gmail.com>
2022-09-16 04:20:06 +0300

    create print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers, from 0 - 9,
+ * except 2 and 4, 
+ * a new line follows
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		if ((num == 50) || (num == 52))
+        {
+            continue;
+            putchar(num);
+        }
+	}
+    putchar('\n');
+}
