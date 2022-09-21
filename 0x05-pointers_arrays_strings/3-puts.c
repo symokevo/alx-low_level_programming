b7678b3f1f419d6dedeb9ba5128460613635e914
zimaster <symokevo@gmail.com>
2022-09-21 04:39:15 +0300

    create puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - prints a string, followed by a new line
+ * @str: string to print
+ * Return: 0.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+
+	}
+
+	_putchar('\n');
+
+}
