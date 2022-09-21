fac5c93ad6f8ed032652f87ff0de715091908a13
zimaster <symokevo@gmail.com>
2022-09-21 04:53:20 +0300

    create print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints (n) elements of an array of integers
+ * @a: array of integers
+ * @n: elements of an array
+ * Return: 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+
+		}
+
+		else
+
+		{
+			printf(", %d", a[i]);
+
+		}
+
+	}
+
+	printf("\n");
+
+}
