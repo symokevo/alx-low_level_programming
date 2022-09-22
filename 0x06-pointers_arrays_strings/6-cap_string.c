449a9bfb08efa6e065fd1ff7401985f3e623ff47
zimaster <symokevo@gmail.com>
2022-09-22 18:28:33 +0300

    create cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * Return: character value
+ */
+char *cap_string(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		while (str[a] >= 'a' && str[a] <= 'z')
+		{
+			if (str[a - 1] == ' ' ||
+			str[a - 1] == '\t' ||
+			str[a - 1] == '\n' ||
+			str[a - 1] == ',' ||
+			str[a - 1] == ';' ||
+			str[a - 1] == '.' ||
+			str[a - 1] == '!' ||
+			str[a - 1] == '?' ||
+			str[a - 1] == '"' ||
+			str[a - 1] == '(' ||
+			str[a - 1] == ')' ||
+			str[a - 1] == '{' ||
+			str[a - 1] == '}' ||
+			a == 0)
+				str[a] = str[a] - 32;
+			a++;
+		}
+	}
+	return (str);
+}
