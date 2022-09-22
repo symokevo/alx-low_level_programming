b845ac721a08f7b8b8ea3367d434013b48e444c1
zimaster <symokevo@gmail.com>
2022-09-22 17:58:27 +0300

    fix strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * _strcat - concatenates two strings.
- * @src: part_1 of the string
- * @dest: part_2 of the string
- * Return: dest pointer
+ * _strncat - a function concatenates two strings
+ * @src: source variable
+ * @n: integer variable
+ * @dest: destination variable
+ * Return: destination string
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
+	strncat(dest, src, n);
 	return (dest);
-
 }
