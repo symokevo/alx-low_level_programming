03490e1a799ebd5f6828e773d8e192f7dd68041e
zimaster <symokevo@gmail.com>
2022-09-22 18:05:18 +0300

    create strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - function compares two strings
+ * s1: var to be compared
+ * s2: var to be compared
+ * Return: zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = strcmp(s1, s2);
+	return (res);
+}
