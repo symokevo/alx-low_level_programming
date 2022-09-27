20cde2f1942a5e460a02c6d6284d1808bb9e929c
zimaster <symokevo@gmail.com>
2022-09-27 20:49:16 +0300

    create set_string.c file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set value of pointer to char.
+ * @to: char pointer
+ * @s: Double pointer to char
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
