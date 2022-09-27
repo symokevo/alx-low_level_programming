319b4233922bdf3380a403ecbdb53effd454e86c
zimaster <symokevo@gmail.com>
2022-09-27 20:41:22 +0300

    create print_chessboard.c file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard -  print chessboard.
+ * @a: chess board array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j, sizeA, sizeB;
+
+	sizeA = (int)sizeof(a);
+	sizeB = 8;
+
+	for (i = 0; i < sizeA; i++)
+	{
+		for (j = 0; j < sizeB; j++)
+		{
+			_putchar(a[i][j]);
+		}
+	_putchar('\n');
+	}
+}
