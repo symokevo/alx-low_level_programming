5087f65778720d31716efee82985c0e98be8a060
zimaster <symokevo@gmail.com>
2022-09-29 05:53:17 +0300

    create fuctorial.c file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the given number
+ * Return: factorial of n and if n is lower than 0, the function should return -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
