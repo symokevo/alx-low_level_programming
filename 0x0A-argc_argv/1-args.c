6f8ccfc187d79920afcf80513ad316ce485faddd
zimaster <symokevo@gmail.com>
2022-10-01 21:30:20 +0300

    create args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector pointer
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
