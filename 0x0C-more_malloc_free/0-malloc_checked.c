9af3acdf5b940e774764a59fbb063b398fd2232b
zimaster <symokevo@gmail.com>
2022-10-07 04:50:25 +0300

    create malloc_checked.c file

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of malloc
+ * Return: a pointer to the allocated memory,
+ * or if malloc fails, 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+c programming repo with excercise code files on 0x0C. C - More malloc, free
