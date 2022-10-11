ff9792562ff779c1f973d78d505ff76318d4d59d
zimaster <symokevo@gmail.com>
2022-10-11 18:55:15 +0300

    create main.c file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+/**
+ * main - print the name of the file it was compiled from,
+ * a new line follows
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return (0);
+}
